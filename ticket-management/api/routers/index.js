e3c0b1847c65f08aa422cda3068fec922231eac5
andyvuong <vuongtrian116@gmail.com>
2024-09-22 18:04:45 -0500

    clearn the resources

--- ticket-management/api/routers/index.js
+++ ticket-management/api/routers/index.js
@@ -1,15 +1,9 @@
 const express = require("express");
-// const usersRouter = require("./users");
-// const moviesRouter = require('./movies');
-// const actorRouter = require('./actor');
+
 const ticketsRouter = require("./tickets");
-// const ticketsGraphRouter = require("./ticketsGraph");
+
 const router = express.Router();
 
-// router.use(process.env.USERS_ROUTE, usersRouter);
 router.use(process.env.TICKETS_ROUTE, ticketsRouter);
-// router.use(process.env.MOVIES_ROUTE, moviesRouter);
-// router.use(process.env.MOVIES_ROUTE, actorRouter);
-// router.use("/graphql", ticketsGraphRouter);
 
 module.exports = router;
