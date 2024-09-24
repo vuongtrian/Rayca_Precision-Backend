0ab689cff9beafb9f2f445b3b99e5209d9dd0380
andyvuong <vuongtrian116@gmail.com>
2024-09-24 13:49:22 -0500

    add swagger

--- ticket-management/app.js
+++ ticket-management/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 require("dotenv").config();
 require("./api/data/db");
 require("./api/data/redisUtil");
-
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const path = require("path");
 const router = require("./api/routers");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./api/controllers/ticketGraphType");
@@ -30,6 +32,14 @@ app.use(
     graphiql: true, // Enable GraphiQL for testing
   })
 );
+
+// Read swagger.json file
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "swagger.json"), "utf8")
+);
+
+// Set up Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const server = app.listen(process.env.PORT, function () {
   console.log(process.env.APP_LISTEN_PORT + server.address().port);
