1b8552c9597585e76d3336f74a719b72982e0308
andyvuong <vuongtrian116@gmail.com>
2024-09-25 15:53:10 -0500

    fix bug at user controller

--- user-management/api/controllers/authenticationController.js
+++ user-management/api/controllers/authenticationController.js
@@ -1,23 +1,32 @@
-const jwt = require('jsonwebtoken');
-const util = require('util');
-const responseUtil = require('../util/responseUtil');
+const jwt = require("jsonwebtoken");
+const util = require("util");
+const responseUtil = require("../util/responseUtil");
 
 const authenticate = function (req, res, next) {
   const response = responseUtil._getForbiddenErrorResponse();
 
   const headerExists = req.headers.authorization;
+  console.log("header " + headerExists);
   if (headerExists) {
     const token = req.headers.authorization.split(process.env.SPACE)[1];
     const verify = util.promisify(jwt.verify);
 
     verify(token, process.env.SECRET_KEY_JWT)
-      .then((decodedToken) => responseUtil._verifiedUser(req, decodedToken, next))
-      .catch((err) => responseUtil._sendReponseWithStatusAndMessage(res, process.env.HTTP_FORBIDDEN_ERROR_STATUS_CODE, process.env.ERROR_TOKEN_EXPIRED))
+      .then((decodedToken) =>
+        responseUtil._verifiedUser(req, decodedToken, next)
+      )
+      .catch((err) =>
+        responseUtil._sendReponseWithStatusAndMessage(
+          res,
+          process.env.HTTP_FORBIDDEN_ERROR_STATUS_CODE,
+          process.env.ERROR_TOKEN_EXPIRED
+        )
+      );
   } else {
     responseUtil._sendReponse(res, response);
   }
 };
 
 module.exports = {
-  authenticate
+  authenticate,
 };
--- user-management/api/controllers/usersController.js
+++ user-management/api/controllers/usersController.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const responseUtil = require('../util/responseUtil');
-const jwt = require('jsonwebtoken');
-const util = require('util');
-const speakeasy = require('speakeasy');
-const qrcode = require('qrcode');
-const {verify} = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const responseUtil = require("../util/responseUtil");
+const jwt = require("jsonwebtoken");
+const util = require("util");
+const speakeasy = require("speakeasy");
+const qrcode = require("qrcode");
+const { verify } = require("jsonwebtoken");
 
 const User = mongoose.model(process.env.USERS_MODEL);
 
@@ -15,7 +15,7 @@ const addOne = function (req, res) {
     username: req.body.username,
     password: req.body.password,
     roles: req.body.roles,
-    email: req.body.email
+    email: req.body.email,
   });
   if (_validateRequest(req)) {
     responseUtil._sendReponseWithStatusAndMessage(
@@ -51,8 +51,16 @@ const login = function (req, res) {
   }
   User.findOne(query)
     .exec()
-    .then(foundUser => responseUtil._checkExistedData(foundUser, response, process.env.ERROR_FIND_USER))
-    .then((existedUser) => responseUtil._checkPasswordUser(existedUser, req.body.password))
+    .then((foundUser) =>
+      responseUtil._checkExistedData(
+        foundUser,
+        response,
+        process.env.ERROR_FIND_USER
+      )
+    )
+    .then((existedUser) =>
+      responseUtil._checkPasswordUser(existedUser, req.body.password)
+    )
     .then((verifiedUser) => _generateToken(verifiedUser))
     .then((token) => responseUtil._getTokenSuccessResponse(token, response))
     .catch((err) => responseUtil._getErrorResponse(err, response))
@@ -62,27 +70,28 @@ const login = function (req, res) {
 const enableMFA = function (req, res) {
   const response = responseUtil._initResponse();
   const secret = _generateSecret(req);
-  console.log(secret.base32);
   let query = {
     mfaSecret: secret.base32,
-    mfaEnabled: true
-  }
+    mfaEnabled: true,
+  };
 
   User.findByIdAndUpdate(req.body.id, query)
-      .exec()
-      .then((updatedUser) =>_generateQrCode(secret))
-      .then(generatedQrCode => responseUtil._getSuccessResponse(generatedQrCode, response))
-      .catch(error => responseUtil._getErrorResponse(error, response))
-      .finally(() => responseUtil._sendReponse(res, response));
-}
+    .exec()
+    .then((updatedUser) => _generateQrCode(secret))
+    .then((generatedQrCode) =>
+      responseUtil._getSuccessResponse(generatedQrCode, response)
+    )
+    .catch((error) => responseUtil._getErrorResponse(error, response))
+    .finally(() => responseUtil._sendReponse(res, response));
+};
 
-const testAuthorization = function (req, res)  {
+const testAuthorization = function (req, res) {
   responseUtil._sendReponseWithStatusAndMessage(
-      res,
-      parseInt(process.env.HTTP_BAD_REQUEST_STATUS_CODE),
-      process.env.ERROR_LOGIN_USER
+    res,
+    parseInt(process.env.HTTP_BAD_REQUEST_STATUS_CODE),
+    process.env.ERROR_LOGIN_USER
   );
-}
+};
 
 const _validateRequest = function (req) {
   return !(req.body && req.body.username && req.body.password);
@@ -105,22 +114,28 @@ const _generateHash = function (password, salt) {
 
 const _generateToken = function (dbUser) {
   const sign = util.promisify(jwt.sign);
-  return sign({ username: dbUser.username, roles: dbUser.roles}, process.env.SECRET_KEY_JWT, {
-    expiresIn: parseInt(process.env.EXPIRED_TIME_JWT),
-  });
+  return sign(
+    { username: dbUser.username, roles: dbUser.roles },
+    process.env.SECRET_KEY_JWT,
+    {
+      expiresIn: parseInt(process.env.EXPIRED_TIME_JWT),
+    }
+  );
 };
 
 const _generateSecret = function (req) {
-  return speakeasy.generateSecret({ name: process.env.SECRET_KEY_MFA + req.user.username });
-}
+  return speakeasy.generateSecret({
+    name: process.env.SECRET_KEY_MFA + req.user.username,
+  });
+};
 
 const _generateQrCode = function (secret) {
   return qrcode.toDataURL(secret.otpauth_url);
-}
+};
 
 module.exports = {
   register: addOne,
   login,
   enableMFA: enableMFA,
-  testAuthorization
+  testAuthorization,
 };
